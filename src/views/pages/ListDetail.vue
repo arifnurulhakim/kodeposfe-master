c75619c78eaa960b8d5f02265003982df11351da
arifnurulhakim <arifnhakim23@gmail.com>
2023-07-18 20:12:50 +0700

    update tampilan

--- src/views/pages/ListDetail.vue
+++ src/views/pages/ListDetail.vue
@@ -56,7 +56,9 @@
       var longitude = response.data.longitude;
       // Create Leaflet map
       map.value = L.map('map').setView([latitude, longitude], 11);
-      L.tileLayer('http://mt0.google.com/vt/lyrs=m&hl=id&x={x}&y={y}&z={z}').addTo(map.value);
+      L.tileLayer('http://mt0.google.com/vt/lyrs=m&hl=id&x={x}&y={y}&z={z}', {
+            attribution: 'Map data &copy; <a href="https://www.google.com/maps">GoogleMaps</a> contributors'
+        }).addTo(map.value);
       var geoJSONLayer = L.geoJSON().addTo(map.value);
       var geometry = JSON.parse(response.data.geojson);
       geoJSONLayer.addData(geometry);
@@ -124,50 +126,56 @@
           </div>
         </div>
       </div>
-      <div id="highlights" class="py-4 px-4 lg:px-8 mx-0 my-6 lg:mx-8">
+      <div id="highlights" class="py-4 px-4 mx-0 md:mx-8">
+        <h1 class="text-900 font-normal mb-2">Wilayah Kode Pos {{ datakodepos.kode_old }} </h1>
         <div class="pb-6 rounded-b">
-          <p class="my-6">Berikut ini adalah informasi untuk kelurahan {{ datakodepos.nama_kecamatan }}, {{ datakodepos.nama_kabupaten }}, {{ datakodepos.nama_provinsi }}, {{ datakodepos.kode_new }}:</p>
-          <div class="block md:flex gap-6">
-            <div class="w-auto max-w-[100%] overflow-x-auto mb-6">
+          <p class="my-6">Berikut ini adalah informasi untuk kelurahan {{ datakodepos.nama_kecamatan }}, {{ datakodepos.nama_kabupaten }}, {{ datakodepos.nama_provinsi }}, {{ datakodepos.kode_old }}:</p>
+          <div class="block md:flex gap-4">
+            <div class="flex justify-content-center col-12 lg:col-6 bg-purple-100 p-0 flex-order-1 lg:flex-order-0" style="border-radius: 8px">
+              <!-- <img src="/demo/images/landing/mockup.svg" class="w-11" alt="mockup mobile" /> -->
+              <div id="map"></div>
+            </div>
+            <br>
+            <div class="w-auto max-w-[100%] overflow-x-auto">
               <center>
-                <img v-bind:src="'data:image/;base64,' + qrcode" style="width: 100px; height: 100px;" />
-                <button class="p-button-rounded border-none ml-5 font-light text-white line-height-2 bg-blue-500" @click="printQRCode">Cetak QR Code</button>
+                <h2 class="text-900 font-normal mb-2">Details</h2>
+                <img id="qrcode" v-bind:src="'data:image/;base64,' + qrcode" />
+                <br>
+                <Button label="Cetak QR Code" @click="printQRCode"  class="p-button-rounded border-none font-light text-white line-height-2 bg-blue-500"></Button>
               </center>
-              <table class="w-full text-sm">
+              <br>
+              <table class="custom-table">
                 <tbody>
-                  <tr class="odd:bg-neutral-100">
+                  <tr class="odd-bg">
                     <td class="font-bold px-6 py-3">Kode Pos Lama</td>
                     <td class="px-6 py-3">{{ datakodepos.kode_old }}</td>
                   </tr>
-                  <tr class="odd:bg-neutral-100">
+                  <tr class="even-bg">
                     <td class="font-bold px-6 py-3">Kecamatan</td>
                     <td class="px-6 py-3">{{ datakodepos.nama_kecamatan }}</td>
                   </tr>
-                  <tr class="odd:bg-neutral-100">
+                  <tr class="odd-bg">
                     <td class="font-bold px-6 py-3">Kota</td>
                     <td class="px-6 py-3">{{ datakodepos.nama_kabupaten }}</td>
                   </tr>
-                  <tr class="odd:bg-neutral-100">
+                  <tr class="even-bg">
                     <td class="font-bold px-6 py-3">Provinsi</td>
                     <td class="px-6 py-3">{{ datakodepos.nama_provinsi }}</td>
                   </tr>
                 </tbody>
               </table>
             </div>
-            <div class="flex justify-content-center col-12 lg:col-6 bg-purple-100 p-0 flex-order-1 lg:flex-order-0" style="border-radius: 8px">
-              <!-- <img src="/demo/images/landing/mockup.svg" class="w-11" alt="mockup mobile" /> -->
-              <div id="map" style="height: 100%; width: 400px;"></div>
-            </div>
           </div>
         </div>
       </div>
-      <div class="py-4 px-4 mx-0 mt-8 lg:mx-8">
+      <div class="py-4 px-4 mx-0 md:mx-8">
+        <h2 class="text-900 font-normal mb-2">Daftar Desa/Kelurahan</h2>
         <DataTable ref="dt" :value="desas" dataKey="id" :paginator="true" :rows="10" :filters="filters" paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown" :rowsPerPageOptions="[5, 10, 25]"
           currentPageReportTemplate="Showing {first} to {last} of {totalRecords} kabupatens" responsiveLayout="scroll">
           <Column field="kode_desa" header="kode Pos Baru" :sortable="true" headerStyle="width:40%; min-width:10rem;">
             <template #body="slotProps">
-                <span class="p-column-title">Kode Pos Baru</span>
-                {{ slotProps.data.kode_new }}
+                      <span class="p-column-title">Kode Pos Baru</span>
+                      {{ slotProps.data.kode_new }}
 </template>
           </Column>
           <Column field="kode_desa" header="kode desa" :sortable="true" headerStyle="width:40%; min-width:10rem;">
@@ -191,50 +199,142 @@
         </DataTable>
       </div>
       <div class="py-4 px-4 mx-0 mt-8 lg:mx-8">
-        <div class="grid justify-content-between">
-          <div class="col-12 md:col-2" style="margin-top: -1.5rem">
-            <a @click="smoothScroll('#home')" class="flex flex-wrap align-items-center justify-content-center md:justify-content-start md:mb-0 mb-3 cursor-pointer">
-              <img :src="logoUrl" alt="footer sections" width="50" height="50" class="mr-2" />
-              <h4 class="font-medium text-3xl text-900">SAKAI</h4>
-            </a>
-          </div>
-          <div class="col-12 md:col-10 lg:col-7">
-            <div class="grid text-center md:text-left">
-              <div class="col-12 md:col-3">
-                <h4 class="font-medium text-2xl line-height-3 mb-3 text-900">Company</h4>
-                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">About Us</a>
-                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">News</a>
-                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Investor Relations</a>
-                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Careers</a>
-                <a class="line-height-3 text-xl block cursor-pointer text-700">Media Kit</a>
-              </div>
-              <div class="col-12 md:col-3 mt-4 md:mt-0">
-                <h4 class="font-medium text-2xl line-height-3 mb-3 text-900">Resources</h4>
-                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Get Started</a>
-                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Learn</a>
-                <a class="line-height-3 text-xl block cursor-pointer text-700">Case Studies</a>
-              </div>
-              <div class="col-12 md:col-3 mt-4 md:mt-0">
-                <h4 class="font-medium text-2xl line-height-3 mb-3 text-900">Community</h4>
-                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Discord</a>
-                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Events<img src="/demo/images/landing/new-badge.svg" class="ml-2" /></a>
-                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">FAQ</a>
-                <a class="line-height-3 text-xl block cursor-pointer text-700">Blog</a>
-              </div>
-              <div class="col-12 md:col-3 mt-4 md:mt-0">
-                <h4 class="font-medium text-2xl line-height-3 mb-3 text-900">Legal</h4>
-                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Brand Policy</a>
-                <a class="line-height-3 text-xl block cursor-pointer mb-2 text-700">Privacy Policy</a>
-                <a class="line-height-3 text-xl block cursor-pointer text-700">Terms of Service</a>
-              </div>
-            </div>
-          </div>
-        </div>
+                            <div class="grid justify-content-between">
+                               
+                                    <div class="copyright">
+                                        <p>© QRKODEPOS. All Rights Reserved.</p>
+                                    </div>
+                                <div class="col-xl-3 col-md-6">
+                                    <div class="social-icons">
+                                        <a href="#" target="_blank">
+                                            <i class="pi pi-facebook"></i>
+                                        </a>
+                                        <a href="#" target="_blank">
+                                            <i class="pi pi-twitter"></i>
+                                        </a>
+                                        <a href="#" target="_blank">
+                                            <i class="pi pi-instagram"></i>
+                                        </a>
+                                        <a href="#" target="_blank">
+                                            <i class="pi pi-linkedin"></i>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                </div>
       </div>
     </div>
   </div>
   <AppConfig simple />
 </template>
 
-
+<style scoped>
+  /* Add your custom CSS styles here */
+  .custom-table {
+    width: 100%;
+    text-align: left;
+    border-collapse: collapse;
+  }
+  .custom-table th,
+  .custom-table td {
+    padding: 10px;
+    border-bottom: 1px solid #ccc;
+  }
+  .odd-bg {
+    background-color: #f0f0f0;
+    /* Light Gray Background */
+  }
+  .even-bg {
+    background-color: #fff;
+    /* White Background */
+  }
+  .social-icons a {
+    margin-right: 10px;
+    color: #333;
+    /* Change color to your preference */
+    font-size: 20px;
+  }
+  /* #hero {
+            background: linear-gradient(0deg, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2)), radial-gradient(77.36% 256.97% at 77.36% 57.52%, #eeefaf 0%, #c3e3fa 100%);
+            height: 700px;
+            overflow: hidden;
+        } */
+  @media screen and (min-width: 768px) {
+    /* #hero {
+                -webkit-clip-path: ellipse(150% 87% at 93% 13%);
+                clip-path: ellipse(150% 87% at 93% 13%);
+                height: 530px;
+            } */
+    #map {
+      height: 100%;
+      width: 500px;
+      border-radius: 10px;
+    }
+    #qrcode {
+      width: 200px;
+      height: 200px;
+      border-radius: 20px;
+      margin-top: 10px;
+      margin-bottom: 10px;
+    }
+  }
+  @media screen and (min-width: 1300px) {
+    #hero>img {
+      position: absolute;
+      max-width: 450px;
+    }
+    #hero>div>p {
+      max-width: 450px;
+    }
+  }
+  @media screen and (min-width: 1000px) {
+    #hero>img {
+      position: absolute;
+      max-width: 450px;
+    }
+    #hero>div>p {
+      max-width: 450px;
+    }
+    #map {
+      height: 100%;
+      width: 500px;
+      border-radius: 10px;
+    }
+    #qrcode {
+      width: 200px;
+      height: 200px;
+      border-radius: 10px;
+    }
+  }
+  @media screen and (max-width: 1300px) {
+    #hero {
+      height: 600px;
+      width: 600px;
+    }
+    #hero>img {
+      position: static;
+      transform: scale(1);
+      margin-left: auto;
+      max-width: 450px;
+      max-height: 450px;
+    }
+    #hero>div {
+      width: 100%;
+    }
+    #hero>div>p {
+      width: 100%;
+      max-width: 100%;
+    }
+    #map {
+      height: 400px;
+      width: 100%;
+      border-radius: 10px;
+    }
+    #qrcode {
+      width: 200px;
+      height: 200px;
+      border-radius: 20px;
+    }
+  }
+</style>
 
